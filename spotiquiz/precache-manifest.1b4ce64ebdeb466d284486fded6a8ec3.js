c3c07b8f870f497657336c48b7aea12ed7f7db30
Maciej <maciej@tnt.local>
2018-10-31 23:34:46 +0100

    Added initial spotiquiz build

---
+++ spotiquiz/precache-manifest.1b4ce64ebdeb466d284486fded6a8ec3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "64e2399b7ab4ebaf141e",
+    "url": "./static/js/main.64e2399b.chunk.js"
+  },
+  {
+    "revision": "2ee028cf68e07acff1af",
+    "url": "./static/js/1.2ee028cf.chunk.js"
+  },
+  {
+    "revision": "64e2399b7ab4ebaf141e",
+    "url": "./static/css/main.a64d5fbf.chunk.css"
+  },
+  {
+    "revision": "7d54e68ca1837aaaad21396156f39f32",
+    "url": "./index.html"
+  }
+];
